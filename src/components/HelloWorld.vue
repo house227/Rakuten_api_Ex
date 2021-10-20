d28599ffd78c0427635ba0e43d1df4dea01d09bc
house227 <house1000227@gmail.com>
2021-10-20 16:02:11 +0900

    一旦形に

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <table>
+      <tr>
+        <th>ランキング</th> <td>{{recipe_rank}}</td>
+      </tr>
+      <tr>
+        <th>タイトル</th><td>{{recipe_title}}</td>
+      </tr>
+      <tr>
+        <th>イメージ</th><td><img :src="recipe_image" alt="" ></td>
+      </tr>
+      <tr>
+        <th>必要食材</th><td v-for="recipe in recipe_material" :key="recipe">{{recipe}}</td>
+      </tr>
+    </table>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data:() => ({
+    recipe_rank:"",
+    recipe_title:"",
+    recipe_image:"",
+    recipe_material:"",
+    recipe:"",
+  }),
+  async mounted(){
+    const url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&applicationId=1098499049918798561"
+
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    for(let i = 0; i < 3; i++){
+
+    }
+
+    const first_recipe = data.result[1]
+    this.recipe_title = first_recipe.recipeTitle
+    this.recipe_image = first_recipe.smallImageUrl
+    this.recipe_material = first_recipe.mediumMaterial
+    this.recipe_rank = first_recipe.rank
+
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
